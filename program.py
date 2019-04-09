06dbf4894b893738880aa5b48934e5e8dd23cdb7
Luke Griffith <luke@griffith.cloud>
2019-04-09 21:45:56 +0100

    adding example code from https://github.com/SebLague/Neural-Network-python

---
+++ program.py
@@ -0,0 +1,28 @@
+import neuralnetwork as nn
+import numpy as np
+import matplotlib.pyplot as plt 
+
+# .npz is a foramt for storing multiple numpy arrays.
+
+with np.load('mnist.npz') as data:
+    training_images = data['training_images']
+    training_labels = data['training_labels']
+
+
+# display the images....
+#plt.imshow(training_images[0].reshape(28,28), cmap = 'gray')
+#plt.show()
+#print(training_labels[0])
+
+
+
+
+layer_sizes = (784,5,10) 
+x = np.ones((layer_sizes[0],1))
+
+import pdb
+
+
+net = nn.NeuralNetwork(layer_sizes)
+net.print_accuracy(training_images,training_labels)
+
