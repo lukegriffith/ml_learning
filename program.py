73361eaaf4416868fd610aaf45f933bea0d743bd
Luke Griffith <luke@griffith.cloud>
2019-04-10 00:02:35 +0100

    tidied up

--- program.py
+++ program.py
@@ -8,19 +8,13 @@ with np.load('mnist.npz') as data:
     training_images = data['training_images']
     training_labels = data['training_labels']
 
-
 # display the images....
 #plt.imshow(training_images[0].reshape(28,28), cmap = 'gray')
 #plt.show()
 #print(training_labels[0])
 
-
-
-
 layer_sizes = (3,5,10) 
 x = np.ones((layer_sizes[0],1))
-
-
 
 net = nn.NeuralNetwork(layer_sizes)
 #net.print_accuracy(training_images,training_labels)
@@ -29,9 +23,5 @@ n1 = net.predict(x)
 net.enable_constant()
 n2 = net.predict(x)
 
-
-
 print(n1)
 print(n2)
-
-
